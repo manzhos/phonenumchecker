d5a46415207289784d9d83d914cc975e67502552
Vladimir Manzhos <manzhos@gmail.com>
2022-05-13 09:02:08 +0200

    Num Checker

--- index.js
+++ index.js
@@ -1,27 +1,60 @@
 const express = require('express')
 const app  = express()
 
-const port = process.env.PORT || 5000
+const port       = process.env.PORT || 5000
+const accountSid = process.env.TWILIO_ACCOUNT_SID
+const serviceSid = process.env.TWILIO_SERVICE_SID
+const authToken  = process.env.TWILIO_AUTH_TOKEN
+const client     = require('twilio')(accountSid, authToken)
 
 app.get('/', (req, res)=>{
-    res.status(200).send({
-        message: "You are on Homepage",
-        info: {
-			phone: "Send verification code through /phone . It contains two params i.e. phonenumber and channel(sms/call)",
-            verify: "Verify the recieved code through /verify . It contains two params i.e. phonenumber and code"
-        }
-    })
+  res.status(200).send({
+    message: "You are on Homepage",
+    info: {
+      phone: "Send verification code through /phone . It contains two params i.e. phonenumber and channel(sms/call)",
+      verify: "Verify the recieved code through /verify . It contains two params i.e. phonenumber and code"
+    }
+  })
 })
 
-app.get('/home', (req, res)=>{
-    res.status(200).send({
-        message: "You are on Homepage",
-        info: {
-			phone: "Send verification code through /phone . It contains two params i.e. phonenumber and channel(sms/call)",
-            verify: "Verify the recieved code through /verify . It contains two params i.e. phonenumber and code"
-        }
-    })
+// Phone Endpoint
+app.get('/phone', (req,res) => {
+  if (req.query.phonenumber) {
+    console.log('Sending the code to phone with entered number');
+      client.verify.services(serviceSid).verifications
+        .create({
+           to: `+${req.query.phonenumber}`,
+           channel: 'sms' // req.query.channel==='call' ? 'call' : 'sms'
+        })
+        .then(verification => console.log(verification.status))
+  } else {
+    res.status(400).send({message: "Wrong phone number :(", phonenumber: req.query.phonenumber})
+  }
 })
+
+// Verify Endpoint
+app.get('/verify', (req, res) => {
+	console.log('Sending the code to phone with entered number', req.query.phonenumber, req.query.code);
+    if (req.query.phonenumber && (req.query.code).length === 6) {
+		console.log('Sending the code to phone with entered number');
+        client.verify.services(serviceSid).verificationChecks
+            .create({
+                to: `+${req.query.phonenumber}`,
+                code: req.query.code
+            })
+            .then(data => {
+                if (data.status === "approved") {
+                    res.status(200).send({
+                        message: "User is Verified!",
+                        data
+                    })
+                }
+            })
+    } else {
+        res.status(400).send({ message: "Wrong phone number or code :(", phonenumber: req.query.phonenumber, data })
+    }
+})
+
 
 app.listen(port,() => {
 	// createService();
